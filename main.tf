0a6f611752d39024deb6ea48c4b8da8b663076f0
SkyRyzrEx <jack.nathan.it@gmail.com>
2026-02-13 20:50:16 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -49,8 +49,8 @@ resource "azurerm_virtual_network" "vnet" {
 
 resource "azurerm_network_security_group" "nsg" {
     name = "tf-learning-nsg"
-    location = "azurerm_resource_group.rg.location
-    resource_group_name = "azurerm_resource_group.rg.name"
+    location = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
     name = "allow-rdp"
